9c30c232c9a07699a40a29f8a3674020738d67be
Tien Nguyen <nguyentien8x@gmail.com>
2023-05-06 00:20:59 +0700

    Init v2

---
+++ apps/bridge/components/layerZero/config.ts
@@ -0,0 +1,7 @@
+const VERSION = '0.0.0-alpha.27-mainnet.9'
+const SHA384 = 'pVqha9VraKgj5nrQMHrVbz4mTagT5Ay6i6Albk2lHZ+w4IbIsERs5sUnZJTz6ERB'
+
+export const LAYER_ZERO_JS = {
+  src: `https://unpkg.com/@layerzerolabs/aptos-bridge-widget@${VERSION}/element.js`,
+  integrity: `sha384-${SHA384}`,
+}
